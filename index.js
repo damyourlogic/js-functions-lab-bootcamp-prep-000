1fbbb9ec3f1ee751ee42ff8611342a5086a48ba4
Amy Jackson <aj81623@gmail.com>
2017-10-24 19:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
 happyHolidaysTo("Jon")
+function happyHolidayTo(holiday, name){
+  return(`Happy ${holiday}, ${name}`)
+}
+happyHolidayTo("Thankgiving, Jon")
